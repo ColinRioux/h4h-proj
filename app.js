372208de11d847cbf8d9d57112d5d75f4d707ed9
Colin Rioux <colin350@gmail.com>
2018-03-03 13:49:18 -0800

    app

---
+++ app.js
@@ -0,0 +1,13 @@
+// Express entry point file
+const express = require('express');
+const app = express();
+
+app.get('/', function(req,res) {
+    res.send('Hi Charles <3');
+});
+
+app.listen(3000, function() {
+    console.log('App running on local host port 3000');
+    console.log('Either localhost:3000');
+    console.log('Or 127.0.0.1:3000');
+});

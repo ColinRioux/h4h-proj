ac438bce0368d9f7df5e3287c71c55657f460f5d
Colin Rioux <colin350@gmail.com>
2018-03-04 02:13:24 -0800

    playing around w search

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,42 +1,55 @@
 $ = require('jquery');
 var fs = require('fs');
-var tmp = JSON.parse(fs.readFileSync('./local/tmp.json', 'utf8'));
 var search = "";
 
 $(document).ready(function() {
-    buildFeed();
+    buildFeed('#feed', false);
     $('#btnfilter').click(function() {
-        filter();
-    })
-    window.setInterval(function() {
-        buildFeed();
-    }, 10000);
+        search = $('#filter').val();
+        buildFeed('#feed', true);
+    });
+    $('#refreshJobs').click(function() {
+        buildFeed('#feed', false);
+    });
 });
 
-function buildFeed() {
-    $('#feed').html('');
+function buildFeed(selector, s) {
+    var tmp = JSON.parse(fs.readFileSync('./local/tmp.json', 'utf8'));
+    $(selector).html('');
     for (var i in tmp.requests) {
         var request = tmp.requests[i];
-        $('#feed').append('<div class="box ' + request.id +'" style="width:50%;"><h1 class="title is-4">'+request.title+'</h1><ul class="bxlc"><li><i class="fas fa-users"></i> Size:' + request.size + '</li><li><i class="fas fa-map-marker"></i> Location: ' + request.location + '</li></ul><br/><p style="padding-top:2%">' + request.description + '</p><br/><ul class="tgclist"></ul></div>');
-        if (typeof request.category === "string") {
-            var a = [];
-            a.push(request.category);
-            request.category = a;
-        } 
-        for (var j in request.category) {
-            $('.' + request.id + '').find('.tgclist').append('<li><div class="jobCategory">' + request.category[j] + '</div></li>');
+        var o = true;
+        if (s) {
+            o = false;
+            for (var t in request.tags) {
+                if (tags[t] == search) {
+                    o = true;
+                } 
+            }
         }
-        $('.' + request.id + '').find('.tgclist').append('<li style="padding-left:30px;"><i class="fas fa-tags"></i></li>');
-        for (var k in request.tags) {
-            $('.' + request.id + '').find('.tgclist').append('<li><div class="jobTag">' + request.tags[k] + '</div></li>');
+        if (o) {
+           $(selector).append('<div class="box ' + request.id +'" style="width:50%;" onclick="expandCard' + request.id + '"><h1 class="title is-4">'+request.title+'</h1><ul class="bxlc"><li><i class="fas fa-users"></i> Size:' + request.size + '</li><li><i class="fas fa-map-marker"></i> Location: ' + request.location + '</li></ul><br/><p style="padding-top:2%">' + request.description + '</p><br/><ul class="tgclist"></ul></div>');
+            if (typeof request.category === "string") {
+                var a = [];
+                a.push(request.category);
+                request.category = a;
+            } 
+            for (var j in request.category) {
+                $(selector).find('.' + request.id + '').find('.tgclist').append('<li><div class="jobCategory">' + request.category[j] + '</div></li>');
+            }
+            $(selector).find('.' + request.id + '').find('.tgclist').append('<li style="padding-left:30px;"><i class="fas fa-tags"></i></li>');
+            for (var k in request.tags) {
+                $(selector).find('.' + request.id + '').find('.tgclist').append('<li><div class="jobTag">' + request.tags[k] + '</div></li>'); 
+            } 
         }
     }
-} 
+}
 
 function filter() {
-    $('.searchfield').html("");
+    $('.searchfield').html('');
     search = $("#filter").val();
-    $('.searchfield').append('<p>' + search + '</p>');
+    //$('.searchfield').append('<p>' + search + '</p>');
+    // buildFeed('.searchfield', true);
     for(var req in tmp.requests){
         var request = tmp.requests[req];
         if(search == "Search..."){
@@ -52,4 +65,8 @@ function filter() {
     }
     
     // $('body').append('<p>' + search + '</p>');
+}
+
+function expandCard(id) {
+
 }

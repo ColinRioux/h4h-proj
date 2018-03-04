0bb95a8c65ac16e93f6399df67323ea20f888340
Colin Rioux <colin350@gmail.com>
2018-03-04 01:19:35 -0800

    fix search

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,11 +1,7 @@
 $ = require('jquery');
 var fs = require('fs');
-var btn = document.getElementById("btnfilter");
-
+var tmp = JSON.parse(fs.readFileSync('./local/tmp.json', 'utf8'));
 var search = "";
-btn.onclick = function() {
-    filter();
-};
 
 $(document).ready(function() {
     buildFeed();
@@ -16,15 +12,11 @@ $(document).ready(function() {
         buildFeed();
     }, 10000);
 });
+
 function buildFeed() {
     $('#feed').html('');
-    var tmp = JSON.parse(fs.readFileSync('./local/tmp.json', 'utf8'));
     for (var i in tmp.requests) {
         var request = tmp.requests[i];
-        // var tags = tmp.requests[i].tags.split(',');
-        // for (var j in tmp.requests[i].category) {
-            
-        // }
         $('#feed').append('<div class="box ' + request.id +'" style="width:50%;"><h1 class="title is-4">'+request.title+'</h1><ul class="bxlc"><li><i class="fas fa-users"></i> Size:' + request.size + '</li><li><i class="fas fa-map-marker"></i> Location: ' + request.location + '</li></ul><br/><p style="padding-top:2%">' + request.description + '</p><br/><ul class="tgclist"></ul></div>');
         if (typeof request.category === "string") {
             var a = [];
@@ -35,26 +27,25 @@ function buildFeed() {
             $('.' + request.id + '').find('.tgclist').append('<li><div class="jobCategory">' + request.category[j] + '</div></li>');
         }
         $('.' + request.id + '').find('.tgclist').append('<li style="padding-left:30px;"><i class="fas fa-tags"></i></li>');
-        // for (var j in tags) {
-        //     $('#feed').append('<li><div class="jobTag">' + tags[j] + '</div></li>');
-        // }
+        for (var k in request.tags) {
+            $('.' + request.id + '').find('.tgclist').append('<li><div class="jobTag">' + request.tags[k] + '</div></li>');
+        }
     }
 } 
 
 function filter() {
-    // $('.searchfield').html("");
-    // var search = "Evac";
-    search = document.getElementById("filter").value;
+    $('.searchfield').html("");
+    search = $("#filter").val();
     $('.searchfield').append('<p>' + search + '</p>');
-    for(var req in data.requests){
-        
+    for(var req in tmp.requests){
+        var request = tmp.requests[req];
         if(search == "Search..."){
-            $('.searchfield').append('<p>' + data.requests[req].title + '</p>');
+            $('.searchfield').append('<p>' + request.title + '</p>');
         } else {
-            var tags = data.requests[req].tags;
+            var tags = request.tags;
             for(var t in tags){
                 if(tags[t] == search){
-                    $('.searchfield').append('<p>' + data.requests[req].title + '</p>');
+                    $('.searchfield').append('<p>' + request.title + '</p>');
                 }
             }
         }

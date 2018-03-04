3aed6d220d4d62863e5633ab0a1b34a047c92c99
Charles <cju@scu.edu>
2018-03-03 23:55:57 -0800

    Filter Working

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,19 +1,37 @@
 $ = require('jquery');
 var fs = require('fs');
-$(document).ready(function() {
-    // var categories = JSON.parse(fs.readFileSync('./local/job_builder.json', 'utf8')).categories;
-    // for (var c in categories) {
-    //     $('#categoriesSelector').append($('<option/>', {
-    //         value: c,
-    //         text: categories[c] 
-    //     }));
-    // }
+var btn = document.getElementById("btnfilter");
+var file = fs.readFileSync('./local/tmp.json', 'utf8')
+var data = JSON.parse(file);
 
-    var file = fs.readFileSync('./local/tmp.json', 'utf8')
-    var data = JSON.parse(file);
+btn.onclick = function() {
+    filter();
+}
+
+var search = "Food";
+
+function filter() {
+    $('.searchfield').html("");
+    // var search = "Evac";
+    search = document.getElementById("filter").value;
     for(var req in data.requests){
         
-        $('body').append('<p>' + data.requests[req].title + '</p>');
+        if(search == ""){
+            $('.searchfield').append('<p>' + data.requests[req].title + '</p>');
+        } else {
+            var tags = data.requests[req].tags;
+            for(var t in tags){
+                if(tags[t] == search){
+                    $('.searchfield').append('<p>' + data.requests[req].title + '</p>');
+                }
+            }
+        }
     }
     
+    // $('body').append('<p>' + search + '</p>');
+}
+
+$(document).ready(function() {
+    filter();
+
 });
